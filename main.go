40d1677ed2d7dca6fc31360921676e9957bef8e0
su1gen <prybylovychav@gmail.com>
2022-06-05 22:07:25 +0300

    Create request for getting posts

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func getPosts() {
+	response, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		fmt.Println("wrong request")
+		return
+	}
+
+	posts, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("error with response")
+		return
+	}
+	//Convert the body to type string
+	postsForPrint := string(posts)
+	fmt.Println(postsForPrint)
+}
 
 func main() {
-	fmt.Println("Hello, NIX Education")
+	getPosts()
 }
